d83024fdce21fb9287c2bebfcd9fcfa1ed2a3fa9
clarkwmcd <clarkwmcd@gmail.com>
2018-02-03 15:33:31 -0500

    took setup JS and moved it back to the HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,42 +55,6 @@ $(document).ready(function() {
 
   //END AUTHENTICATION
 
-  //START SETUP PAGE JS
 
-  var organizationForm = $("#organization-form");
-  var restaurantForm = $("#restaurant-form");
-  var buttons = $("#buttongroup");
-  var submit = $("#submit");
-
-  organizationForm.hide();
-  restaurantForm.hide();
-  submit.hide();
-
-  $("#donor").on("click", function(event) {
-    console.log("donor clicked")
-    restaurantForm.show();
-    submit.show();
-    buttons.hide();
-    //hide #organization-form
-    //show #restaurant-form
-    //hide buttons
-  });
-
-  $("#requester").on("click", function(event) {
-    console.log("requester clicked")
-    organizationForm.show();
-    submit.show();
-    buttons.hide();
-    //show #organization-form
-    //hide #restaurant-form
-    //hide buttons
-  });
-
-  submit.on("click", function(event) {
-    var name = $("#restaurant").val();
-    var address = $("restuarant-address").val();
-    var database = firebase.database()
-    database.ref().push(name);
-  });
 
 });
