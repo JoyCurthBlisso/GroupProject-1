2ee8f4b018035fc512f2076e5840132acd4dd0bd
Roxana Milea <roxanamilea@Roxanas-MacBook-Air.local>
2018-02-04 16:59:43 -0500

    merge master with updated app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  //Materialize CSS parralax function
+  // $('.parallax').parallax();
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBR4mFvJiYUIQDIVoDzKCWZyxyeJ-C5rCw",
@@ -25,6 +28,7 @@ $(document).ready(function() {
       // Update the current user UID.
 
       currentUid = user.uid;
+      console.log(currentUid);
 
 
     } else {
@@ -70,6 +74,10 @@ $(document).ready(function() {
     console.log("donor clicked")
     restaurantForm.show();
     submit.show();
+
+    organizationForm.hide();
+    // $("#organization").val("");
+    // $("#organization-address").val("");
     buttons.hide();
     //hide #organization-form
     //show #restaurant-form
@@ -80,6 +88,10 @@ $(document).ready(function() {
     console.log("requester clicked")
     organizationForm.show();
     submit.show();
+
+    restaurantForm.hide();
+    // $("#restaurant").val("");
+    // $("#restaurant-address").val("");
     buttons.hide();
     //show #organization-form
     //hide #restaurant-form
@@ -88,10 +100,9 @@ $(document).ready(function() {
 
   submit.on("click", function(event) {
     var name = $("#restaurant").val();
-    var address = $("restuarant-address").val();
-    var database = firebase.database();
-    goToHome();
-    // database.ref().push(name);
+    var address = $("#restaurant-address").val();
+    var database = firebase.database()
+    database.ref().push(name).push(address);
   });
 
   //END SETUP PAGE JS
