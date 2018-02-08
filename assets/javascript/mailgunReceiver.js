fb6ff9892e498b7ef53ece600ebb93bb309bfc08
bterrier <joann_enos@verizon.net>
2018-02-07 21:47:29 -0500

    Update mailgun.js

--- assets/javascript/mailgunReceiver.js
+++ assets/javascript/mailgunReceiver.js
@@ -1,38 +1,78 @@
 $(document).ready(function() {
 
-function receiverEmail(restaurant, address, donationType, numOfItems, pickUpTime) {
-	var queryURL = "https://api.mailgun.net/v3/";
-	var hdrVal = "Basic " + btoa("api:key-ef72536d6301ae8ec42279773af8eaf9");
-	var text = `<p>This is to confirm that you have agreed to pick up this donation today: </p> <p>Donation: </p>  <p>Pick up window and time:  </p> <p>Thank you so much for your KarmaFoodBank charity! </p> <pstyle="text-align:center;">— KarmaFoodBank </p>`
+	$("#donation").on("click",function(event) {
+		if ($("#filled-in-box").is(":checked")) {
+			console.log("checked");
 
-	$.ajax({
-		url: "https://us-central1-empower-hope.cloudfunctions.net/api/mailgun-api/sandboxc502a7a2dae748469de9804c3742317f.mailgun.org/messages",
-		method: "POST",
-		headers: {"Authorization": hdrVal},
-		data: {
-			from: "jojoenos@gmail.com",
-			to: "jojoenos@gmail.com",
-			subject: "Hello from KarmaFoodBank",
-			html: text	
+			// function receiverEmail(restaurant, address, donationType, numOfItems, pickUpTime) {
+			function receiverEmail() {
+				var queryURL = "https://api.mailgun.net/v3/";
+				var hdrVal = "Basic " + btoa("api:key-ef72536d6301ae8ec42279773af8eaf9");
+				var donationType = $("#donateFood").text();
+				var pickUpTime = $("#pickUpTime").text();
+				var numOfItems = $("#number").text();
+				var text = `<p>This is to confirm that you have agreed to pick up this donation today: </p> <ul> <p> <li> Donation: `+ donationType +` </li> </p> <p> <li> Number of items: `+ numOfItems +` </li> </p> <p> <li> Pick up time: `+ pickUpTime +` </li> </p> <p>Thank you so much for your KarmaFoodBank charity! </p> <p style="text-align:center;">— KarmaFoodBank </p>`
 
-			},
+				$.ajax({
+					url: "https://us-central1-empower-hope.cloudfunctions.net/api/mailgun-api/sandboxc502a7a2dae748469de9804c3742317f.mailgun.org/messages",
+					method: "POST",
+					headers: {"Authorization": hdrVal},
+					data: {
+						from: "jojoenos@gmail.com",
+						to: "jojoenos@gmail.com",
+						subject: "Hello from KarmaFoodBank",
+						html: text	
+						},
 
-		}).then(function(response){
-		   console.log(response);
-		});
-	}
+					}).then(function(response){
+					   console.log(response);
+					});
+				}
 
-	$(".submit").click(function() {
-		// var toUser = (from Firebase);
-		var restaurant = $("#donateFood").val().trim();
-		var address = $("#menuNumber").val().trim();
-		var donationType = $("#value").val().trim();
-		var numOfItems = 
-		var pickUpTime = $("#pickUp").val().trim;
+			// receiverEmail(restaurant, address, donationType, numOfItems, pickUpTime) //this order matters and matches donatorEmail function argument list.
+			receiverEmail(); //this order matters and matches donatorEmail function argument list.
 
-		donatorEmail(restaurant, address, donationType, numOfItems, pickUpTime) //this order matters and matches donatorEmail function argument list.
+			function donatorAcceptedEmail() {
+				var queryURL = "https://api.mailgun.net/v3/";
+				var hdrVal = "Basic " + btoa("api:key-ef72536d6301ae8ec42279773af8eaf9");
+				var donationType = $("#donateFood").text();
+				var pickUpTime = $("#pickUpTime").text();
+				var numOfItems = $("#number").text();
+				var text = `<p>This is to confirm that your donation has been accepted. </p> <ul> <p> <li> Donation: `+ donationType +` </li> </p> <p> <li> Number of items: `+ numOfItems +` </li> </p><p> <li> Pickup time:  `+ pickUpTime +` </li> </p> <p>Thank you again for your generosity! </p> <p style="text-align:center;"> — KarmaFoodBank </p>`
 
-	})
+				console.log(donationType);
+	
+				$.ajax({
+					url: "https://us-central1-empower-hope.cloudfunctions.net/api/mailgun-api/sandboxc502a7a2dae748469de9804c3742317f.mailgun.org/messages",
+					method: "POST",
+					headers: {"Authorization": hdrVal},
+					data: {
+						from: "jojoenos@gmail.com",
+						to: "jojoenos@gmail.com",
+						subject: "Hello from KarmaFoodBank",
+						html: text	
+
+						},
+
+					}).then(function(response){
+
+					});
+
+				}
+
+			donatorAcceptedEmail() 
+
+		} else {
+			var checkCheckBox = "Please confirm you've checked your item." // logic to display error message on form
+			alert("Please confirm you've checked your item.");
+		};
+
+	});
 
 });
+
+
+
+
+
 
