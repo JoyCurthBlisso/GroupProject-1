08316722667f8d8d494322989bee3f0b849593b0
bterrier <joann_enos@verizon.net>
2018-02-08 20:52:22 -0500

    Update mailgun.

--- assets/javascript/mailgunReceiver.js
+++ assets/javascript/mailgunReceiver.js
@@ -1,6 +1,18 @@
 //FOR THE ANGELS.HTML PAGE (CONFRIM MESSENGER THEY ARE PICKING UP FOOD)
 
 $(document).ready(function() {
+	// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyBR4mFvJiYUIQDIVoDzKCWZyxyeJ-C5rCw",
+			authDomain: "fir-project-ca268.firebaseapp.com",
+			databaseURL: "https://fir-project-ca268.firebaseio.com",
+			projectId: "fir-project-ca268",
+			storageBucket: "",
+			messagingSenderId: "60944439745"
+		};
+
+		firebase.initializeApp(config); 
+		var database = firebase.database();		
 
 	$("#donation").on("click",function(event) {
 		if ($("#filled-in-box").is(":checked")) {
@@ -13,6 +25,7 @@ $(document).ready(function() {
 				var donationType = $("#donateFood").text();
 				var pickUpTime = $("#pickUpTime").text();
 				var numOfItems = $("#number").text();
+				var userEmail = firebase.auth().currentUser.email;
 				var text = `<p>This is to confirm that you have agreed to pick up this donation today: </p> <ul> <p> <li> Donation: `+ donationType +` </li> </p> <p> <li> Number of items: `+ numOfItems +` </li> </p> <p> <li> Pick up time: `+ pickUpTime +` </li> </p> <p>Thank you so much for your KarmaFoodBank charity! </p> <p style="text-align:center;">— KarmaFoodBank </p>`
 
 				$.ajax({
@@ -21,13 +34,14 @@ $(document).ready(function() {
 					headers: {"Authorization": hdrVal},
 					data: {
 						from: "jojoenos@gmail.com",
-						to: "jojoenos@gmail.com",
+						to: "jojoenos@gmail.com", //userEmail
 						subject: "Hello from KarmaFoodBank",
 						html: text	
 						},
 
 					}).then(function(response){
 					   console.log(response);
+					   console.log("Email", userEmail);
 					});
 				}
 
@@ -41,6 +55,7 @@ $(document).ready(function() {
 				var pickUpTime = $("#pickUpTime").text();
 				var numOfItems = $("#number").text();
 				var text = `<p>This is to confirm that your donation has been accepted. </p> <ul> <p> <li> Donation: `+ donationType +` </li> </p> <p> <li> Number of items: `+ numOfItems +` </li> </p><p> <li> Pickup time:  `+ pickUpTime +` </li> </p> <p>Thank you again for your generosity! </p> <p style="text-align:center;"> — KarmaFoodBank </p>`
+				var userEmail = firebase.auth().currentUser.email;
 
 				console.log(donationType);
 	
@@ -57,16 +72,37 @@ $(document).ready(function() {
 						},
 
 					}).then(function(response){
-
 					});
-
 				}
 
 			donatorAcceptedEmail() 
 
 		} else {
-			var checkCheckBox = "Please confirm you've checked your item." // logic to display error message on form
-			alert("Please confirm you've checked your item.");
+			var checkCheckBox = "Please select at least one checkbox." // logic to display error message on form
+			// alert("Please confirm you've checked your item.");
+			$("#filled-in-box").validate({
+				rules: {
+					check: {
+						required: true,
+					}
+				},
+				messages: {
+					check: {
+						required: "Select at least one checkbox."
+					}
+				},
+				errorElement : 'p',
+				errorPlacement: function(error,element) {
+					var placement = $(element).data("error");
+					if (placement) {
+						$(placement).append(error) 				
+					} else {
+						error.insertAfter(element);
+					}
+				}
+			});
+
+
 		};
 
 	});
