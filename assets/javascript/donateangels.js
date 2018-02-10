7dc9659374747d75d505b5653641da24d7ee4be9
Joy Curth <joy.curth@comcast.net>
2018-02-10 17:03:55 -0500

    trying to fix mailgun issue

--- assets/javascript/donateangels.js
+++ assets/javascript/donateangels.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   $('.parallax').parallax();
 
 
-
   var config = {
     apiKey: "AIzaSyBR4mFvJiYUIQDIVoDzKCWZyxyeJ-C5rCw",
     authDomain: "fir-project-ca268.firebaseapp.com",
@@ -47,6 +46,43 @@ $(document).ready(function() {
       };
 
 
+      var userEmail = "jojoenos@gmail.com";
+    var donationType = $("#donateFood").val().trim();
+    var numPeople = $("#menuNumber").val().trim();
+    var donationValue = $("#value").val().trim();
+    var pickUpTime = $("#pickUp").val().trim();
+
+    function donatorEmail() {
+      var queryURL = "https://api.mailgun.net/v3/";
+      var hdrVal = "Basic " + btoa("api:key-ef72536d6301ae8ec42279773af8eaf9");
+      var userEmail = "jojoenos@gmail.com";
+    
+      var donationType = $("#donateFood").val().trim();
+    
+    
+      var text = `<p>Thank you so much for your generous donation.</p> <p>Your benevolence is appreciated and you will reap the karmic rewards of that. </p>  <p>This is to confirm that you have offered this to the community today: <ul> </p> <p> <li> Donation: `+ donationType +` </li> </p> <p> <li> Approximate value:  `+ donationValue + `</li> </p> <li> For number of people: `+ numPeople +` </li> <p> <li> Pick up time: `+ pickUpTime +` </li> </ul> </p>  <p>You will receive an email if an organization in need will be able to benefit from this donation. </p> <p>Thank you again for your KarmaFoodBank charity! </p> <p style='text-align:center'>— KarmaFoodBank </p>`
+
+        $.ajax({
+          url: "https://us-central1-empower-hope.cloudfunctions.net/api/mailgun-api/sandboxc502a7a2dae748469de9804c3742317f.mailgun.org/messages",
+          method: "POST",
+          headers: {"Authorization": hdrVal},
+          data: {
+            from: "jojoenos@gmail.com",
+            to: userEmail,
+            subject: "Hello from KarmaFoodBank",
+            html: text
+
+            },
+
+          }).then(function(response){
+             console.log(response);
+             console.log(userEmail);
+          });
+
+      }
+
+      donatorEmail(); 
+
       $("#donateFood").val("");
       $("#menuNumber").val("");
       $("#value").val("");
@@ -79,4 +115,5 @@ $(document).ready(function() {
 
 
 
-}); 
+});
+
